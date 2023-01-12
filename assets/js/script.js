7ca3b3b24ecff0d08585ab3913c5246dca720630
OneDeadFox <tjosephb.pro@gmail.com>
2023-01-12 09:08:06 -0800

    created html skeleton with ids and classes

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+//TODO: Be able to search for a specific movie title
+    //1. Take user input and search stream availability api for cinema that matches.
+        //a. Place a saftey mechanisim on this fetch that prevents more than 50 fetches per day.
+    //2. Take response from stream ability and store it in local storage as "testQuery".
+        //a. Use testQuery during testing and production
+//TODO: Have search results displayed on the page.
+    //1. Display search results as img cards with class="title".
+    //2. Display search in rows with 4 columns per row.
+    //3. Have modal with title details appear upon card click.
+    //4. Have save button in modal to add title to watch list.
+        //a. Likely, we sould add a save put hovering over the one of the bottom corners of title while it is a search result card.
+
+//TODO: Have a carousel of top 10 movies under search bar onload.
+    //1. Display top ten api results in carousel.
+    //2. Have modal with title details appear upon card click.
+    //3. Have save button in modal to add title to watch list.
+//TODO: Have ability to save movies to a watch list.
+    //1. Add selected title and its details to array in local storage.
+//TODO: Have ability to view watch list.
+    //1. Have watch list modal appear upon clicking watch list button.
+        //a. location of watch-list button needs to be determined.
+
+//Global Variables---------------------------------------------
+
+
+
+
+//Event Listeners----------------------------------------------
+
+
+
+
+//Functions----------------------------------------------------
+
+//stream availability api fetch function
+function getUserQuery(input){
+
+}
+
+//imdb api fetch function
+function  getTopTen(){
+
+}
+
+//place carousel card items in carousel
+function populateCarousel(array)
+
+//Launch modal for title information
+function titleDetails(title){
+
+}
+
+//save buttonfunction
+function updateWatchList(title){
+
+}
